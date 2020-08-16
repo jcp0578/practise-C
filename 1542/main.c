8a352c23cc0f52a5eb9f9097524a305e2a7656a3
JCP@Laptop <jcp0578@gmail.com>
2020-08-16 22:58:11 +0800

    AC but slower than before

--- 1542/main.c
+++ 1542/main.c
@@ -9,16 +9,13 @@ int longestAwesome(char *s)
         return len;
     }
     int indexSave[1024] = {};
-    memset(indexSave, 0, sizeof(int) * 1024);
+    memset(indexSave, 0xff, sizeof(int) * 1024);
+    indexSave[0] = 0;
     int countTemp = 0;
     int res = 0;
 
     for (int i = 0; i < len; i++)     {
         countTemp ^= (1 << (s[i] - '0'));
-        if(countTemp == 0) {
-            res = i + 1;
-            continue;
-        }
         for (int j = 0; j <= 10; j++) {
             int checkTemp;
             if (j == 10) {
@@ -26,22 +23,16 @@ int longestAwesome(char *s)
             } else {
                 checkTemp = countTemp ^ (1 << j);
             }
-
-            
-            if(checkTemp == 0) {
-                res = i + 1;
-                break;
-            } else {
-                int matchIndex = indexSave[checkTemp];
-                if (matchIndex != 0) {
-                    int resTemp = i - matchIndex + 1;
-                    res = (res < resTemp) ? resTemp : res;
-                }
+      
+            int matchIndex = indexSave[checkTemp];
+            if (matchIndex != -1) {
+                int resTemp = i - matchIndex + 1;
+                res = (res < resTemp) ? resTemp : res;
             }
         }
-        int saveIndex = countTemp;
-        if (indexSave[saveIndex] == 0) {
-            indexSave[saveIndex] = i + 1;
+        
+        if (indexSave[countTemp] == -1) {
+            indexSave[countTemp] = i + 1;
         }
     }
     return res;
